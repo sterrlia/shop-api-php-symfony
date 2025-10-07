80b7e150b92303c51332b04031f8a694f599dd25
sterrlia <dev@sterrlia.com>
2025-10-07 17:47:11 +0400

    fix autowire, makefile and workflow update

--- Makefile
+++ Makefile
@@ -5,3 +5,9 @@ lint:
 
 format:
 	@vendor/bin/php-cs-fixer fix
+
+check-format:
+	@vendor/bin/php-cs-fixer fix --dry-run --diff
+
+check: check-format lint
+	php bin/console lint:container
--- src/Infrastructure/Repository/AbstractEntityRepository.php
+++ src/Infrastructure/Repository/AbstractEntityRepository.php
@@ -11,7 +11,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
  *
  * @template-extends ServiceEntityRepository<T>
  **/
-class AbstractEntityRepository extends ServiceEntityRepository
+abstract class AbstractEntityRepository extends ServiceEntityRepository
 {
     /**
      * @param list<int> $ids
